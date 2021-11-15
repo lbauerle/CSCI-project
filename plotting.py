f17ae7765609aad97d1cc711f1f77179230dcd7b
Lauren Bauerle <laba3466@colorado.edu>
2021-11-15 20:24:35 +0000

    Creating plotting library and added skeletons of functions

---
+++ plotting.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def timecontourplot(timedata, axisdata, contourdata, axis, maxint, cmap, imagetype='png'):
+    """
+    
+    """
+    return figure
+
+
+def twodimcontourplot(axis1data, axis2data, contourdata, axis1, axis2, maxint, cmap, imagetype='png'):
+    """
+    
+    """
+    return figure
+
+
+def timelasercontourplot(timedata, axisdata, contourdata, laserdata, axis, maxint, cmap, imagetype='png'):
+    """
+    
+    """
+    return figure
+
+def twodimlasercontourplot(axis1data, axis2data, contourdata, timedata, laserdata, axis1, axis2, maxint, cmap, imagetype='png'):
+    """
+    
+    """
+    return figure
+
+
+def makegif(figurefolder):
+    
+    return gif
