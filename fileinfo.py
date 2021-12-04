2782bcc57cf55cc3122fbc8b2c59a3dec8ed2d4e
Lauren Bauerle <laba3466@colorado.edu>
2021-12-04 15:50:04 +0000

    implemented numpy to read in grid information, testing to see if faster than base python

--- fileinfo.py
+++ fileinfo.py
@@ -57,15 +57,24 @@ def gridsize(densfilename):
         symmetric axes. A symmetric z-axis would have values ranging from 
         +/- sizez.
     """
-    start = time.process_time()
+    start1 = time.time()
     file_handle = open(densfilename, mode='r')
     sizeline = file_handle.readlines()[1]
     data = sizeline.split()
     sizex = abs(float(data[1]))
     sizey = abs(float(data[2]))
     sizez = abs(float(data[3]))
-    print(str(time.process_time() - start) + ' seconds')
-
+    print(str(time.time() - start1) + ' seconds')
+    
+    start2 = time.time()
+    sizex2 = np.genfromtxt(densfilename, max_rows = 1, dtype = int, 
+                           usecols = 8, delimiter = ' ')
+    sizey2 = np.genfromtxt(densfilename, max_rows = 1, dtype = int, 
+                           usecols = 12, delimiter = ' ')
+    sizez2 = np.genfromtxt(densfilename, max_rows = 1, dtype = int, 
+                           usecols = 16, delimiter = ' ')
+    print(str(time.time() - start2) + ' seconds')
+    
     return sizex, sizey, sizez
 
 def gridspacing(densfilename):
