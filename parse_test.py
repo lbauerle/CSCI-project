6311dcf744189231daa718846bfcb0283d283348
Lauren Bauerle <laba3466@colorado.edu>
2021-12-04 22:59:13 +0000

    added polarization to parser

--- parse_test.py
+++ parse_test.py
@@ -18,12 +18,16 @@ def input_parser():
                         action='store',
                         required=False,
                         help="This is the denisty to be parsed.")
-    counter_inputs = parser.parse_args()
-    
+   
     parser.add_argument('-l', '--laser', type=str,
                         action='store',
                         required=False,
                         help="This is the laser to be parsed.")
+    
+    parser.add_argument('-p', '--pol', type=str,
+                        action='store',
+                        required=False,
+                        help="This is the polarization of the laser.")
     counter_inputs = parser.parse_args()
 
     if counter_inputs.config:
@@ -31,9 +35,11 @@ def input_parser():
         config.read("./config.ini")
         density_file = config['DENSITY']['density_name']
         laser_file = config['LASER']['laser_name']
+        polarization = config['LASER']['polarization']
         
-        return density_file, laser_file
+        return density_file, laser_file, polarization
     else:
         density_file = counter_inputs.dens
         laser_file = counter_inputs.laser
-        return density_file, laser_file
+        polarization = counter_inputs.pol
+        return density_file, laser_file, polarization
