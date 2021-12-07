5141115beb384f551057db23c64a7b0976c58a8c
Lauren Bauerle <laba3466@colorado.edu>
2021-12-07 00:32:37 +0000

    for some reason run.sh wasn't put into this branch even though it's on main

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# so we don't get an error
+source /opt/conda/etc/profile.d/conda.sh
+
+# activate conda environment
+#conda activate swefs
+
+# run code with arguments
+python density_difference.py --config config.ini > output.txt
+
+# deactivate conda environment
+#conda deactivate
