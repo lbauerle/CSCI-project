c0d39701957b3551da9b8755c49a9978ff17a62e
ajdominic <ajdominiciii@outlook.com>
2021-12-10 19:47:40 +0000

    updated according to the PEP8 standards and made sure that the code was modular

--- density_difference.py
+++ density_difference.py
@@ -18,70 +18,60 @@ def input_parser():
     ------------
     """
 
-    parser = argparse.ArgumentParser(
-        description='')
+    # specifying help options
+    parser = argparse.ArgumentParser(description='')
 
-    parser.add_argument('-c', '--config', type=str,
-                    action='store',
-                    required=False,
-                    help="This is the config file.")
+    parser.add_argument('-c', '--config', type=str, action='store',
+                        required=False, help="This is the config file.")
 
-    parser.add_argument('-d', '--dens', type=str,
-                        action='store',
-                        required=False,
-                        help="This is the directory containing the density file to be parsed.")
+    parser.add_argument('-d', '--dens', type=str, action='store',
+                        required=False, help="This is the directory" +
+                        " containing the density file to be parsed.")
 
-    parser.add_argument('-l', '--laser', type=str,
-                        action='store',
-                        required=False,
-                        help="This is the directory for the laser file to be parsed.")
+    parser.add_argument('-l', '--laser', type=str, action='store',
+                        required=False, help="This is the directory" +
+                        "for the laser file to be parsed.")
 
-    parser.add_argument('-p', '--pol', type=str,
-                        action='store',
+    parser.add_argument('-p', '--pol', type=str, action='store',
                         required=False,
                         help="This is the polarization of the laser.")
 
-    parser.add_argument('-m', '--maxiter', type=str,
-                        action='store',
-                        required=False,
-                        help="This is the maximum number of iterations to be processed.")
+    parser.add_argument('-m', '--maxiter', type=str, action='store',
+                        required=False, help="This is the maximum number" +
+                        "of iterations to be processed.")
 
-    parser.add_argument('-i', '--iter', type=str,
-                        action='store',
+    parser.add_argument('-i', '--iter', type=str, action='store',
                         required=False,
                         help="This is the iteration step for outputs.")
 
-    parser.add_argument('-f', '--plane', type=str,
-                        action='store',
+    parser.add_argument('-f', '--plane', type=str, action='store',
                         required=False,
                         help="This is the plane to plot density difference.")
 
-    parser.add_argument('-n', '--int', type=str,
-                        action='store',
+    parser.add_argument('-n', '--int', type=str, action='store',
                         required=False,
                         help="This is the integration method to use.")
 
-    parser.add_argument('-t', '--time', type=str,
-                        action='store',
+    parser.add_argument('-t', '--time', type=str, action='store',
                         required=False,
                         help="This is units to use for time.")
 
-    parser.add_argument('-b', '--cmap', type=str,
-                        action='store',
+    parser.add_argument('-b', '--cmap', type=str, action='store',
                         required=False,
                         help="This is the colormap to use for plotting.")
 
-    parser.add_argument('-s', '--save', type=str,
-                        action='store',
+    parser.add_argument('-s', '--save', type=str, action='store',
                         required=False,
                         help="This is directory to save plots in.")
 
-    parser.add_argument('-v', '--level', type=str,
-                        action='store',
+    parser.add_argument('-v', '--level', type=str, action='store',
                         required=False,
-                        help="This is maximum intensity levle to use for the contour plots.")
+                        help="This is maximum intensity level" +
+                        "to use for the contour plots.")
+
     calc_inputs = parser.parse_args()
 
+    # parsing through the arguments in the config file
     if calc_inputs.config:
         config = configparser.ConfigParser()
         config.read("./config.ini")
@@ -152,7 +142,7 @@ def density_difference(dens_data1, dens_data2, integration_type, grid_spacing):
     return densdata_xy
 
 
-def density_difference_plot(density_directory, index, xx, yy, densdata_xy):
+def dens_diff_plt(density_directory, index, xx, yy, densdata_xy):
     """
     This function calls the other two functions in the file in order
     to create the density difference plots.
@@ -189,19 +179,20 @@ def density_difference_plot(density_directory, index, xx, yy, densdata_xy):
 
 
 def laser_plt(density_directory, laser_file_name, polarization,
-                                  index, xx, yy, densdata_xy):
+              index, xx, yy, densdata_xy):
     """
     This function plots the density difference with the laser and point
     during the pulse at which the density difference is taken.
     Parameters
-    ------------------
+    ----------
     density_directory : string
         This input is the full path of the .dx file from which to get data.
+
     laser_file_name : string
         This input is the full path of the laser file from which to get data.
-    Returns
-    ------------------
 
+    Returns
+    -------
     """
     # get laser information
     laser_amplitude = fi.laser_data(laser_file_name, polarization)
@@ -251,13 +242,54 @@ def laser_plt(density_directory, laser_file_name, polarization,
     plt.show()
 
 
-def density_difference_calc(density_directory):
+def movie(figure_directory, file_type, fps):
     """
-    Inputs
+    This function turns the plots of the density difference to a movie
+    in time. Note: The order of the movie will coincide with the
+    alphabetical order of the density or density difference plots.
+
+    Parameters
+    ----------
+    figure_directory : string
+        This input is the full path of the .png saved plots.
+
+    file_type: string
+        This is the file type for the plots. The default is '.png'.
+
+    fps : int
+        This is the frames per second. The default value is 400.
+
     Returns
+    -------
+    This function saves a .gig file to a specified directory
     """
-    # directory = "./N2+/output_iter"
-    density_file = f'{density_directory}/td.0000000/density.dx'
+    images = []
+    for file_name in os.listdir(figure_directory):
+        if file_name.endswith(file_type):
+            file_path = os.path.join(figure_directory, file_name)
+            images.append(imageio.imread(file_path))
+
+    imageio.mimsave(f'{figure_directory}\\density_difference_movie.gif',
+                    images, fps=fps)
+
+
+def main():
+    # get all of the inputs from the input_parser function
+    inputs = list(input_parser())
+    dens_dir = inputs[0]
+    laser_file = inputs[1]
+    polarization = inputs[2]
+    max_iteration = inputs[3]
+    iteration_step = inputs[4]
+    plane = inputs[5]
+    int_method = inputs[6]
+    time_units = inputs[7]
+    cmap = inputs[8]
+    level_max = inputs[9]
+    save_directory = inputs[10]
+
+    # identifying the reference density files
+    density_file = f'{dens_dir}/td.0000000/density.dx'
 
     num_x, num_y, num_z = fi.num_grid_points(density_file)
     size_x, size_y, size_z = fi.grid_size(density_file)
@@ -271,52 +303,22 @@ def density_difference_calc(density_directory):
     y = [np.linspace(-size_y, size_y, num=num_y)]
     xx, yy = np.meshgrid(x, y)
 
-    # looping through the data files; for now we will just work with one file
+    # iterating through the available files in the directory
     for i in range(0, 300, 200):
         print('working on iteration ' + str(i))
         ii = str(i)
         iii = ii.zfill(7)
 
-        Density2 = f'{density_directory}/td.{iii}/density.dx'
+        Density2 = f'{dens_dir}/td.{iii}/density.dx'
         dens_data2 = fi.density_data(Density2)
         dens_data2 = np.reshape(dens_data2, (num_x, num_y, num_z))
 
-        # densdata2 = np.genfromtxt(Density2, skip_header=7, skip_footer=5)
-        # dens_data2 = np.array(densdata2)
-
+        # subtracting dens_data2 from the reference dens_data
         densdata_xy = density_difference(dens_data, dens_data2, "riemann", dz)
 
-        density_difference_plot(density_directory, i, xx, yy, densdata_xy)
-        laser_plt(density_directory, 'N2+/td.general/laser',
-                  'x', i, xx, yy, densdata_xy)
-
-
-def movie(figure_directory, file_type, fps):
-    """
-    This function turns the plots of the density difference to a movie
-    in time. Note: The order of the movie will coincide with the
-    alphabetical order of the density or density difference plots.
-
-    Parameters
-    ----------
-    figure_directory : string
-        This input is the full path of the .png saved plots.
-    file_type: string
-        This is the file type for the plots. The default is '.png'.
-    fps : int
-        This is the frames per second. The default value is 400.
-    """
-    images = []
-    for file_name in os.listdir(figure_directory):
-        if file_name.endswith(file_type):
-            file_path = os.path.join(figure_directory, file_name)
-            images.append(imageio.imread(file_path))
-    imageio.mimsave(f'{figure_directory}\\density_difference_movie.gif', images, fps=fps)
-
-
-def main():
-    density_directory, laser_file, polarization, max_iteration, iteration_step, plane, integration_method, time_units,  cmap, level_max, save_directory = input_parser()
-    density_difference_calc(density_directory)
+        dens_diff_plt(dens_dir, i, xx, yy, densdata_xy)
+        laser_plt(dens_dir, 'N2+/td.general/laser', 'x', i, xx, yy,
+                  densdata_xy)
 
 
 if __name__ == '__main__':
